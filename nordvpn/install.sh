2606f82d7afee4c88a554bbbf1ac4240e7405f1a
Zheng Kai <zhengkai@gmail.com>
2018-05-27 13:53:23 +0800

    up

--- nordvpn/install.sh
+++ nordvpn/install.sh
@@ -2,13 +2,9 @@
 cd $(dirname `readlink -f $0`)
 . ./name.inc.sh
 
-sudo docker network create --subnet=192.168.100.0/24 vpn >/dev/null
-
 sudo docker run \
 	--name $DOCKER_NAME \
 	--cap-add=NET_ADMIN \
-	--net vpn \
-	--ip 192.168.100.100 \
 	--dns 103.86.96.100 \
 	--dns 103.86.99.100 \
 	--restart=always \
--- nordvpn/run.sh
+++ nordvpn/run.sh
@@ -12,8 +12,12 @@ chmod 666 tun
 /usr/sbin/openvpn --daemon nordvpn --config /etc/openvpn/config.conf
 # /usr/sbin/openvpn --config /etc/openvpn/config.conf
 
+date
+
 cat /etc/resolv.conf
+
+sleep 5
 
 ifconfig
 
-/usr/sbin/danted
+/usr/sbin/danted >/dev/null 2>&1
